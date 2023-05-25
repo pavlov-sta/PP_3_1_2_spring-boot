86fd7301eba08cbd3c62eabb8d6905906f4b7305
pavlo <pavlov-sta@mail.ru>
2023-05-25 12:14:22 +0300

    Initial commit

---
+++ src/test/java/ru/pavlov/springboot/Pp312SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pavlov.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
